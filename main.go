6c5536f4e89efa2f8f1ad32b23eed2fdf2a4c380
Monster2035f <113020306+Monster2035f@users.noreply.github.com>
2022-09-14 11:15:42 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import(
+	"fmt"
+	"os"
+)
+
+func main(){
+	discord_client_secret := "8dyfuiRyq=vVc3RRr_edRk-fK__JItpz"
+	var a = "initial"
+	fmt.Println(a)
+
+	var b,c int =	1,2
+	fmt Println(b,c)
+
+	var d=true
+	fmt.Println(d)
+	var e int
+	fmt.Println(e)
+
+	//opps I added a secret 
+
+	awsToken := os.Getenv("AWS_TOKEN")
+	f := "apple"
+	fmt.Println(f)
+}
