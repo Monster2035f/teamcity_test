6ce30c6ab9406b19824a1f0173d25c4ae57e70f8
Monster2035f <113020306+Monster2035f@users.noreply.github.com>
2022-11-17 18:54:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
-// Header file for input output functions
-#include <iostream>// Header file for input output functions
 #include <iostream>
 using namespace std;
   
-aws_access_key_id="aghalgkgljkashdlgkah"
-  aws_access_key="4354i36mh4h4n7m"
-  aws_secret_key="kdpdppphp8f8pdpdp884dpd34phphp9kdkfm4fmfglvk"
 
-// Main() function: where the execution of program begins
 int main()
 {
-    // prints hello world
+   
     cout << "Hello World";
   
     return 0;
